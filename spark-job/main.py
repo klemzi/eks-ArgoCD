ea1c93314a6da5742d29db02fb74476532410d01
Clement Dike <dikeclementu@gmail.com>
2023-08-17 22:19:23 -0600

    replace sample code

--- spark-job/main.py
+++ spark-job/main.py
@@ -1,8 +1,9 @@
 from pyspark.sql import SparkSession
 spark = SparkSession.builder \
     .master("local[1]") \
-    .appName("demo-job") \
+    .appName("my-test-spark-job") \
     .getOrCreate()
 
 df=spark.range(100)
 print(df.sample(0.06).collect())
+# Output: [Row(id=0), Row(id=2), Row(id=17), Row(id=25), Row(id=26), Row(id=44), Row(id=80)]
